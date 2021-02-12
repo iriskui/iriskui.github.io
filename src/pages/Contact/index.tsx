fc7d5c7d99536efab34aac0becc9351fbc1c632b
caoyange <caoyange2013@outlook.com>
2021-02-12 16:17:48 +0800

    feat: contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,5 +1,12 @@
-const WorkPage = () => {
-  return <div>contact</div>;
+import styles from './style.less';
+
+const Contact = () => {
+  return (
+    <div className={styles.contact}>
+      <p>Email: Iriskui@outlook.com</p>
+      <p>Weibo: 苏葵</p>
+    </div>
+  );
 };
 
-export default WorkPage;
+export default Contact;
